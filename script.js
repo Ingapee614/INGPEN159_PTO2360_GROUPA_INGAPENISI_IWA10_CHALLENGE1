1016c06e02d4b7278cb5969987fbd5d77fb0c1b9
Ingapee614 <132918785+Ingapee614@users.noreply.github.com>
2023-10-17 18:23:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -74,17 +74,23 @@ console.log('ID change:', holidays[6].id)
 console.log('Name change:',  copied)
 console.log('Date change:', correctedDate)
 
-const firstHolidayTimestamp = Math.min (
-    holidays[0].date.getTime,
-    holidays[1].date.getTime,
-    holidays[2].date.getTime,
-    holidays[3].date.getTime,
-    holidays[4].date.getTime,
-    holidays[5].date.getTime,
-    holidays[6].date.getTime,
-    holidays[7].date.getTime,
-    holidays[8].date.getTime,
-)
+
+
+const firstHolidayTimestamp =Math.min [
+    holidays[0].date.getTime(),
+    holidays[1].date.getTime(),
+    holidays[2].date.getTime(),
+    holidays[3].date.getTime(),
+    holidays[4].date.getTime(),
+    holidays[5].date.getTime(),
+    holidays[6].date.getTime(),
+    holidays[7].date.getTime(),
+    holidays[8].date.getTime()
+];
+
+console.log(firstHolidayTimestamp);
+
+
 
 const lastHolidayTimestamp = Math.max (
     holidays[0].date.getTime,
@@ -97,7 +103,6 @@ const lastHolidayTimestamp = Math.max (
     holidays[7].date.getTime,
     holidays[8].date.getTime,
 )
-
 const firstDay = firstHolidayTimestamp.getDate;
 const firstMonth = firstHolidayTimestamp.getMonth;
 const lastDay = lastHolidayTimestamp.getDate;
@@ -106,8 +111,8 @@ const lastMonth = lastHolidayTimestamp.getMonth;
 console.log(`${firstDay}/${firstMonth}/${currentYear}`);
 console.log(`${lastDay}/${lastMonth}/${currentYear}`);
 
-/*const randomHoliday = holidays.date[Math.random];
-console.log(randomHoliday)*/
+const randomHoliday = holidays.date[Math.random];
+console.log(randomHoliday)
 
 
 
